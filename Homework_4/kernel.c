5ad01448c894088540138eb37c38e2f8cc9fcc0b
lborowczak <lborowczak@luc.edu>
2015-11-23 17:32:09 -0600

    added more functionality

--- Homework_4/kernel.c
+++ Homework_4/kernel.c
@@ -35,7 +35,81 @@ enum vga_color {
 	COLOR_LIGHT_BROWN = 14,
 	COLOR_WHITE = 15,
 };
+
+//copied from http://www.osdever.net/bkerndev/Docs/keyboard.htm
+unsigned char scancode[128] =
+{
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
+  '9', '0', '-', '=', '\b',	/* Backspace */
+  '\t',			/* Tab */
+  'q', 'w', 'e', 'r',	/* 19 */
+  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
+    0,			/* 29   - Control */
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
+ '\'', '`',   0,		/* Left shift */
+ '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
+  'm', ',', '.', '/',   0,				/* Right shift */
+  '*',
+    0,	/* Alt */
+  ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+  '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+  '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
+};
+
+//Copied from http://wiki.osdev.org/Inline_Assembly/Examples
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    asm volatile ( "inb %1, %0" : "=a"(ret) : "Nd"(port) );
+    /* TODO: Is it wrong to use 'N' for the port? It's not a 8-bit constant. */
+    /* TODO: Should %1 be %w1? */
+    return ret;
+}
+
+static inline void outb(uint16_t port, uint8_t val)
+{
+    asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+    /* TODO: Is it wrong to use 'N' for the port? It's not a 8-bit constant. */
+    /* TODO: Should %1 be %w1? */
+}
+
+//Copied from http://wiki.osdev.org/Text_Mode_Cursor
+/* void update_cursor(int row, int col)
+  * by Dark Fiber
+  */
+ void update_cursor(int row, int col)
+ {
+    unsigned short position=(row*80) + col;
  
+    // cursor LOW port to vga INDEX register
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, (unsigned char)(position&0xFF));
+    // cursor HIGH port to vga INDEX register
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, (unsigned char )((position>>8)&0xFF));
+}
+
 uint8_t make_color(enum vga_color fg, enum vga_color bg) {
 	return fg | bg << 4;
 }
@@ -74,18 +148,23 @@ void terminal_initialize() {
 	}
 }
 
-//scroll the text on the terminal by one line
-void terminal_scroll(){
-	for (size_t y = 1; y < VGA_HEIGHT; y++) {
+//scroll the text on the terminal by l lines
+void terminal_scroll(size_t l) {
+	for (size_t y = l; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
-			const size_t newIndex = (y - 1) * VGA_WIDTH + x;
+			const size_t newIndex = ((y - l) * VGA_WIDTH ) + x;
 			terminal_buffer[newIndex] = terminal_buffer[index];
-		
+		}
+	}
+	for (size_t y = VGA_HEIGHT - l; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t index = (y * VGA_WIDTH) + x;
+			terminal_buffer[index] = make_vgaentry(' ', terminal_color);
 		}
 	}
 }
- 
+
 void terminal_setcolor(uint8_t color) {
 	terminal_color = color;
 }
@@ -94,16 +173,36 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = make_vgaentry(c, color);
 }
- 
+
+
 void terminal_putchar(char c) {
+	if (c == 0) return;
 	if (c == '\n')
 	{
 		terminal_column = 0;
 		if ((terminal_row + 1) == VGA_HEIGHT)
 		{
-			terminal_scroll();
+			terminal_scroll(1);
 		}
 		else ++terminal_row;
+	}
+	else if (c == '\b') //handle backspace
+	{
+		if (terminal_column == 0)
+		{
+			if (terminal_row != 0)
+			{
+				terminal_column = VGA_WIDTH - 1;
+				terminal_row--;
+				terminal_putentryat(' ', terminal_color, terminal_column, terminal_row);
+				
+			}
+		}
+		else
+		{
+			terminal_column--;
+			terminal_putentryat(' ', terminal_color, terminal_column, terminal_row);
+		}
 	}
 	else {
 		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
@@ -112,11 +211,12 @@ void terminal_putchar(char c) {
 			terminal_column = 0;
 			if ((terminal_row + 1) == VGA_HEIGHT)
 			{
-				terminal_scroll();
+				terminal_scroll(1);
 			}
 			else ++terminal_row;
 		}
 	}
+	update_cursor(terminal_row, terminal_column);
 }
  
 void terminal_writestring(const char* data) {
@@ -126,45 +226,31 @@ void terminal_writestring(const char* data) {
 }
 
 
-//Copied from http://wiki.osdev.org/Inline_Assembly/Examples
-static inline uint8_t inb(uint16_t port)
-{
-    uint8_t ret;
-    asm volatile ( "inb %1, %0" : "=a"(ret) : "Nd"(port) );
-    /* TODO: Is it wrong to use 'N' for the port? It's not a 8-bit constant. */
-    /* TODO: Should %1 be %w1? */
-    return ret;
-}
-
-static inline void outb(uint16_t port, uint8_t val)
-{
-    asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
-    /* TODO: Is it wrong to use 'N' for the port? It's not a 8-bit constant. */
-    /* TODO: Should %1 be %w1? */
-}
-
 //copied from http://wiki.osdev.org/PS/2_Keyboard
-
+//made to only write 1 code per keypress using http://wiki.osdev.org/%228042%22_PS/2_Controller
 char getScancode()
 {
-	char c=0;
-	do {
-		if(inb(0x60)!=c)
+	outb(0x20, 0x20);
+	char c = 0;
+	while (true)
+	{
+		if (inb(0x64) & 0x01) //only read if the input buffer is full
 		{
-			c=inb(0x60);
-			if(c>0)
+			c = inb(0x60); //read byte from PS/2 keyboard
+			if(c > 0)
 			{
 				return c;
 			}
 		}
-	}while(1);
+	}
 }
-/*
+
 char getchar()
 {
-	return scancode[getScancode()+1];
+	return scancode[getScancode()];
 }
-*/
+
+
 
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kernel_main. */
@@ -172,15 +258,9 @@ extern "C" /* Use C linkage for kernel_main. */
 void kernel_main() {
 	/* Initialize terminal interface */
 	terminal_initialize();
- 
-	/* Since there is no support for newlines in terminal_putchar
-         * yet, '\n' will produce some VGA specific character instead.
-         * This is normal.
-         */
+	//loop forever to echo key presses back
 	while (1)
 	{
-		terminal_writestring("this is a test\n");
-		terminal_writestring("is it working?\n");
-		terminal_writestring("I can't tell.\n");
+		terminal_putchar(getchar());
 	}
 }
